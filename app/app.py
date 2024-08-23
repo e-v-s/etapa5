79547bea982a18e632441b38779293eae299826b
evs <evoraleite@gmail.com>
2024-08-23 16:33:16 +0300

    add config file

--- app/app.py
+++ app/app.py
@@ -11,4 +11,6 @@ async def variables():
 	variables = dict()
 	variables['version'] = os.environ.get('APP_VERSION')
 	variables['token'] = os.environ.get('TOKEN')
+	variables['bancodev'] = os.environ.get('BANCODEV')
+	variables['config'] = open('/app/config-dev.yml', 'r').read()
 	return {'variables': variables}
