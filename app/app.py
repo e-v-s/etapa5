6529aef6c776b314d782004698cfe3961506e23d
evs <evoraleite@gmail.com>
2024-08-23 15:52:32 +0300

    add env variables

--- app/app.py
+++ app/app.py
@@ -1,6 +1,14 @@
+import os
 from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
 async def root():
 	return {"message":"Helloooooooooow"}
+
+@app.get("/variables")
+async def variables():
+	variables = dict()
+	variables['version'] = os.environ.get('APP_VERSION')
+	variables['token'] = os.environ.get('TOKEN')
+	return {'variables': variables}
